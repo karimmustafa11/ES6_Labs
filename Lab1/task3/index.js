9f41fb3126562f9ce4390205f79810add14b2cb6
karimmustafa <karimmustafa495@gmail.com>
2025-02-07 14:14:50 +0200

    Lab1 Tasks

---
+++ Lab1/task3/index.js
@@ -0,0 +1,29 @@
+var fruits = ["apple", "strawberry", "banana", "orange", "mango"];
+
+var stringFruits = fruits.every(function (fruit) {
+    return typeof fruit === "string"
+})
+
+console.log(stringFruits)
+
+var StartsWithA = fruits.some(function (fruit) {
+    return fruit.startsWith("a");
+});
+
+console.log(StartsWithA)
+
+var filteredFruits = fruits.filter(function (fruit) {
+    return fruit.startsWith("b") || fruit.startsWith("s");
+})
+
+console.log(filteredFruits)
+
+var likedFruits = fruits.map(function (fruit) {
+    return `I like ${fruit}`;
+})
+
+likedFruits.forEach(function (liked) {
+    console.log(liked);
+})
+
+
